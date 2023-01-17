da8f3d148922cb604429189525242cca6b8724e3
Fahd Yousri <fahdyousri@gmail.com>
2023-01-17 22:26:11 +0000

    - add ingredient model, table and factory - remove UserFactory

---
+++ database/factories/IngredientFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Ingredient>
+ */
+class IngredientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        fake()->addProvider(new \FakerRestaurant\Provider\en_US\Restaurant(fake()));
+
+        return [
+            'name' => fake()->meatName(),
+            'stock' => fake()->numberBetween(100, 50000),
+            'recommended-stock' => fake()->numberBetween(10000, 50000),
+        ];
+    }
+}
