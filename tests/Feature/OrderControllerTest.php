e5ad0345eda4bf72e39a755cc73c6d8abdfa3cb3
Fahd Yousri <fahdyousri@gmail.com>
2023-01-18 23:55:20 +0000

    add tests for order

---
+++ tests/Feature/OrderControllerTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Enums\OrderStatus;
+use App\Models\Ingredient;
+use App\Models\Order;
+use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class OrderControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_order_fails_on_unavailable_product_id()
+    {
+        $this->json('POST', route('orders.store'), [
+            'products' => [
+                ['product_id' => 1, 'quantity' => 2],
+            ],
+        ])->assertUnprocessable();
+    }
+
+    public function test_order_fails_on_wrong_quantity()
+    {
+        Order::factory()->create(['id' => 1]);
+        
+        $this->json('POST', route('orders.store'), [
+            'products' => [
+                ['product_id' => 1, 'quantity' => 'x'],
+            ],
+        ])->assertUnprocessable();
+    }
+}
