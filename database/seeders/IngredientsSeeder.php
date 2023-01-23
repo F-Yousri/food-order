632518e4b4e128df36ce2c07f265b09e9930ae45
Fahd Yousri <fahdyousri@gmail.com>
2023-01-23 12:40:46 +0000

    run pint

--- database/seeders/IngredientsSeeder.php
+++ database/seeders/IngredientsSeeder.php
@@ -7,13 +7,12 @@ use Illuminate\Database\Seeder;
 
 class IngredientsSeeder extends Seeder
 {
-
     public function run()
     {
         Ingredient::insert([
-            ['name' => 'Beef', 'stock' => 20000, 'recommended_stock' => 20000,],
-            ['name' => 'Cheese', 'stock' => 5000, 'recommended_stock' => 5000,],
-            ['name' => 'Onion', 'stock' => 1000, 'recommended_stock' => 1000,],
+            ['name' => 'Beef', 'stock' => 20000, 'recommended_stock' => 20000],
+            ['name' => 'Cheese', 'stock' => 5000, 'recommended_stock' => 5000],
+            ['name' => 'Onion', 'stock' => 1000, 'recommended_stock' => 1000],
         ]);
     }
 }
